e32e9d60c311e34df9c5280cffe66b5fad3345b6
Bunks <busk_1@yahoo.com>
2021-08-22 02:41:04 -0400

    added slash command for ping pong

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS);
 const client = new Client({ intents: myIntents });
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+];
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -11,6 +18,12 @@ client.once("ready", () => {
 
 client.on("interactionCreate", async (interaction) => {
   if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  }
 });
 
 client.on("message", (msg) => {
