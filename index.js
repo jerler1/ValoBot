c00b6ea12ff1f5f842f5f4fa81895f4cd0327d53
Bunks <busk_1@yahoo.com>
2021-08-22 02:03:56 -0400

    added dotenv

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+require("dotenv").config();
+const { Client, Intents } = require("discord.js");
+
+const myIntents = new Intents();
+myIntents.add(
+  Intents.FLAGS.DIRECT_MESSAGES,
+  Intents.FLAGS.GUILD_PRESENCES,
+  Intents.FLAGS.GUILD_MEMBERS,
+  Intents.FLAGS.GUILDS
+);
+const client = new Client({ intents: myIntents });
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
