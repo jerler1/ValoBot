0c683373bf40cc3351fe7b74fec55698768738ad
Bunks <busk_1@yahoo.com>
2021-08-22 03:21:35 -0400

    added dot env

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+
 const token = process.env.TOKEN;
 const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
@@ -9,7 +11,7 @@ const commands = [
   new SlashCommandBuilder()
     .setName("ping")
     .setDescription("Replies with pong!"),
-];
+].map((command) => command.toJSON());
 
 const rest = new REST({ version: "9" }).setToken(token);
 
