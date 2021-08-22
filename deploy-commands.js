3c58645de867b6d885e4c52bd9fe5c1f101ba1aa
Bunks <busk_1@yahoo.com>
2021-08-22 03:03:58 -0400

    finished adding a deploy commands file which can deploy a ping command

--- deploy-commands.js
+++ deploy-commands.js
@@ -10,3 +10,19 @@ const commands = [
     .setName("ping")
     .setDescription("Replies with pong!"),
 ];
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
+
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
