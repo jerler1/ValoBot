c2a244a29f6722e81bb097cd1414381ce384b4b9
Bunks <busk_1@yahoo.com>
2021-08-23 16:39:53 -0400

    added guild and guild messages intent

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 require("dotenv").config();
 const { Client, Intents } = require("discord.js");
-const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const myIntents = new Intents();
 myIntents.add(Intents.FLAGS.GUILDS);
+myIntents.add(Intents.FLAGS.GUILD_MESSAGES);
+myIntents.add(Intents.FLAGS.GUILD_PRESENCES);
+myIntents.add(Intents.FLAGS.GUILD_MEMBERS);
+myIntents.add(Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS);
+myIntents.add(Intents.FLAGS.GUILD_MESSAGE_TYPING);
+myIntents.add(Intents.FLAGS.GUILD_MESSAGE_REACTIONS);
+myIntents.add(Intents.FLAGS.DIRECT_MESSAGES);
+
 const client = new Client({ intents: myIntents });
-
-
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("interactionCreate", async (interaction) => {
-  console.log(interaction);
-  if (!interaction.isCommand()) return;
+// client.on("interactionCreate", async (interaction) => {
+//   console.log(interaction);
+//   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
+//   const { commandName } = interaction;
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  }
-});
+//   if (commandName === "ping") {
+//     await interaction.reply("Pong!");
+//   }
+// });
 
-client.on("message", (msg) => {
-  console.log("This is working");
+client.on("messageCreate", (msg) => {
+  console.log(msg);
   if (msg.content === "ping" || msg.content === "!!ping") {
     msg.reply("pong");
+  }
+  if (msg.content === "ying") {
+    msg.reply("yang");
   }
 });
 
